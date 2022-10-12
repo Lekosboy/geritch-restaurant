3b7e25ba5392bbda7be44e8b7e819f34abd3914f
Lekos <supremo.lekos@gmail.com>
2022-10-12 11:58:17 +0100

    Added Gallery section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -6,7 +6,7 @@ import './Laurels.css';
 
 const AwardCard = ({ award: { imgUrl, title, subtitle }}) => (
   <div className='app__laurels_awards-card'>
-    <img src={imgUrl} alt="award" />
+    <img src={imgUrl} alt="awards" />
     <div className='app__laurels_awards-card_content'>
       <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p>
       <p className='p__cormorant'>{subtitle}</p>
@@ -15,7 +15,7 @@ const AwardCard = ({ award: { imgUrl, title, subtitle }}) => (
 );
 
 const Laurels = () => (
-  <div className='app__bg app__wrapper section__padding id="awards'>
+  <div className='app__bg app__wrapper section__padding' id="awards">
     <div className='app__wrapper_info'>
       <SubHeading title="Awards & recognition" />
       <h1 className='headtext__cormorant'>Our Laurels</h1>

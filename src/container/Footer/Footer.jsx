032017691103743ec6dd0ddb33886325c4a97de3
Lekos <supremo.lekos@gmail.com>
2024-02-24 16:17:21 +0000

    Changed footer copyright

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,42 +1,50 @@
-import React from 'react';
-import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import {FooterOverlay, Newsletter} from '../../components';
-import { images } from '../../constants';
-import './Footer.css';
+import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div className='app__footer section__padding'>
+  <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
 
-    <div className='app__footer-links'>
-      <div className='app__footer-links_contact'>
-        <h1 className='app__footer-headtext'>Contact Us</h1>
-        <p className='p__opensans'>15 Princess St, M8U 6EB, Manchester</p>
-        <p className='p__opensans'>+44 7700 900151</p>
-        <p className='p__opensans'>+44 7700 900702</p>
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">15 Princess St, M8U 6EB, Manchester</p>
+        <p className="p__opensans">+44 7700 900151</p>
+        <p className="p__opensans">+44 7700 900702</p>
       </div>
-      <div className='app__footer-links_logo'>
+      <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
-        <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others.”</p>
-        <img src={images.spoon} alt="spoon" className='spoon__img' style={{ marginTop: '15' }} />
-        <div className='app__footer-links_icons'>
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others.”
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: "15" }}
+        />
+        <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
           <FiInstagram />
         </div>
       </div>
-      <div className='app__footer-links_work'>
-        <h1 className='app__footer-headtext'>Working Hours</h1>
-        <p className='p__opensans'>Monday-Friday:</p>
-        <p className='p__opensans'>08:00 am - 12:00am</p>
-        <p className='p__opensans'>Saturday-Sunday:</p>
-        <p className='p__opensans'>07:00am - 11:00pm</p>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00am - 11:00pm</p>
       </div>
     </div>
-    <div className='footer__copyright'>
-      <p className='p__opensans'>2022 Crazy Rafa. All Rights reserved</p>
+    <div className="footer__copyright">
+      <p className="p__opensans">2024 Duty Task. All Rights reserved</p>
     </div>
   </div>
 );
